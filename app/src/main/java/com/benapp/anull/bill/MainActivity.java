1b75b8171a380d2e97b1e53a0ddcd9d84b3f809f
Ben Horn <benhorenn@gmail.com>
2018-03-05 14:38:01 +0200

    design changes, and improve the delete option

--- app/src/main/java/com/benapp/anull/bill/MainActivity.java
+++ app/src/main/java/com/benapp/anull/bill/MainActivity.java
@@ -37,6 +37,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
@@ -84,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
         bulidPage();
         changePage(false);
         bulidAlertOne();
-        bulidPopUp();
+        bulidPopUp(null, "", 0, 0);
     }
 
     View popup =null;
@@ -96,10 +97,9 @@ public class MainActivity extends AppCompatActivity {
             popup = getLayoutInflater().inflate(R.layout.popup, null);
             popup = popup.findViewById(R.id.popupid);
 
-            //        LayoutInflater inflater = (LayoutInflater)    this.getSystemService(LAYOUT_INFLATER_SERVICE);
-//            popup = inflater.inflate(R.layout.popup,
-//                    (ViewGroup) findViewById(R.id.popupid));
         }
+
+
     }
 
 
@@ -142,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void bulidAlertOne(){
+
         alertDialogBuilder = new AlertDialog.Builder(this ,R.style.MyDialogTheme)
                 .setPositiveButton(android.R.string.ok, null) //Set to null. We override the onclick
                 .setNegativeButton(android.R.string.cancel, null)
@@ -216,7 +217,7 @@ public class MainActivity extends AppCompatActivity {
                             alertDialogBuilder.dismiss();
                             updateSummrayScreen();
 //                            bulidAlertOne();
-                            bulidPopUp();
+                            bulidPopUp(null, "",0,0);
                         }
                     }
                 });
@@ -227,12 +228,37 @@ public class MainActivity extends AppCompatActivity {
     /**
      * build the pop up
      */
-    private void bulidPopUp(){
+    private void bulidPopUp(ArrayList<String> nams, String dcs, int amnt, double prc){
 
         poptpLayout.removeView(popup);
         popup=null;
         popupcreate();
         poptpLayout.addView(popup);
+
+
+        if(prc != 0) {
+            TextView p = popup.findViewById(R.id.Price);
+            p.setText(prc + "");
+        }
+        if(amnt != 0) {
+            TextView a = popup.findViewById(R.id.Amount);
+            a.setText(amnt + "");
+        }
+        if(!dcs.isEmpty()) {
+            TextView d = popup.findViewById(R.id.Description);
+            d.setText(dcs + "");
+        }
+        if(nams != null) {
+            for (String nm : nams){
+                TextView d = popup.findViewById(R.id.Name);
+                d.setText(nm);
+                addTinyName(d);
+            }
+        }
+
+
+
+
 
         dropdown();
 
@@ -264,8 +290,6 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception e){
                     p.setTextColor(Color.RED);}
             }
-
-
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             public void onTextChanged(CharSequence s, int start, int before, int count) {}
@@ -282,6 +306,8 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> names = new ArrayList<String>();
         View child;
         LinearLayout namescroll = (LinearLayout) poptpLayout.findViewById(R.id.tinynames);
+        if(namescroll == null) return names;
+
         for(int i=0; i<namescroll.getChildCount(); i++){
             child = namescroll.getChildAt(i);
             TextView tv = (TextView) child.findViewById(R.id.textView);
@@ -483,6 +509,10 @@ public class MainActivity extends AppCompatActivity {
             friendIndex=-1;
 
         }
+
+        for(int i=0; i<friends.size(); i++)
+            if(Double.parseDouble(friends.get(i)[1]) == 0)
+                friends.remove(i--);
     }
 
 
@@ -528,6 +558,10 @@ public class MainActivity extends AppCompatActivity {
             total = sumlay.findViewById(R.id.totalBill);
         }
 
+        sum = sum *10;
+        sum = (int) sum;
+        sum = sum/10;
+
         TextView sumview = (TextView) total.findViewById(R.id.friendsum);
         sumview.setText(sum+"");
 
@@ -571,55 +605,86 @@ public class MainActivity extends AppCompatActivity {
     private void addItem(ArrayList<String> names,String description , String amount, String price){
         LinearLayout items = (LinearLayout) findViewById(R.id.itemContainer);
 
-        View child = getLayoutInflater().inflate(R.layout.item, null);
+         final View child = getLayoutInflater().inflate(R.layout.item, null);
 
         TextView f = (TextView) child.findViewById(R.id.first);
         TextView s = (TextView) child.findViewById(R.id.second);
         TextView t = (TextView) child.findViewById(R.id.third);
         TextView fo = (TextView) child.findViewById(R.id.four);
+
         String name="";
+        for(String nm : names)
+            name +=nm+", ";
+
+        name = name.substring(0, name.length()-2);
+
+        f.setText(name);
+
+//        for(int i=1; i<names.size(); i++){
+//           TextView na = new TextView(this);
+//
+//            na.setTextAppearance(this, R.style.textstyle);
+//
+//
+//            na.setText(", "+names.get(i));
+//            na.setTextSize(18);
+//
+//            LinearLayout itmnm = child.findViewById(R.id.itemname);
+//            itmnm.addView(na);
+//        }
+
+        s.setText(description);
+        t.setText(amount);
+        fo.setText(price);
+
+//        String count =  ""+items.getChildCount();
 
 
         child.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public boolean onLongClick(final View v) {
+            public boolean onLongClick(View view) {
+                Log.d("Ben","long");
+//                view.getLayoutParams().width = 44
+                LinearLayout par = (LinearLayout) child.getParent();
+                par.removeView(child);
+
+                String names = ((TextView) child.findViewById(R.id.first)).getText().toString();
+                String dsc = ((TextView) child.findViewById(R.id.second)).getText().toString();
+                String amou = ((TextView) child.findViewById(R.id.third)).getText().toString();
+                String pri = ((TextView) child.findViewById(R.id.four)).getText().toString();
+
+//                refreshFriendsList(ArrayList<String> names, String desc, int amount, double price
+
+                String[] arr  =names.split(",");
+                ArrayList<String> nms = new ArrayList<String>();
+
+                Log.d("Ben", ""+names);
+                Log.d("Ben", ""+ Arrays.toString(arr));
+                Log.d("Ben", ""+arr.length);
+
+
+                for(int i=0; i<arr.length; i++)
+                    nms.add(arr[i].trim());
+
+
+                refreshFriendsList(nms, dsc, Integer.parseInt(amou.trim()), Double.parseDouble(pri.trim())*-1);
+                updateSummrayScreen();
+
+
+                namesforList = new ArrayList<String>();
+                for(int i=0; i<friends.size(); i++)
+                    namesforList.add(friends.get(i)[0]);
+                bulidPopUp(nms, dsc, Integer.parseInt(amou.trim()), Double.parseDouble(pri.trim()));
+                alertDialogBuilder.show();
+                dropdown();
+
 
 
                 return true;
             }
         });
 
-
-        f.setText(names.get(0));
-
-        for(int i=1; i<names.size(); i++){
-           TextView na = new TextView(this);
-
-            na.setTextAppearance(this, R.style.textstyle);
-
-
-            na.setText(", "+names.get(i));
-            na.setTextSize(18);
-
-            LinearLayout itmnm = child.findViewById(R.id.itemname);
-            itmnm.addView(na);
-        }
-
-        s.setText(description);
-        t.setText(amount);
-        fo.setText(price);
-
-        String count =  ""+items.getChildCount();
-
         items.addView(child);
-//        Log.d("Ben","add item");
-//        Log.d("Ben","size: "+count);
-
-
-
-
-
-
     }
 
 
@@ -633,7 +698,7 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0; i<friends.size(); i++)
             namesforList.add(friends.get(i)[0]);
 
-        bulidPopUp();
+        bulidPopUp(null, "", 0, 0);
 
         alertDialogBuilder.show();
         dropdown();
@@ -708,245 +773,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-
-    /**
-     * Stack Blur v1.0 from
-     * http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
-     * Java Author: Mario Klingemann <mario at quasimondo.com>
-     * http://incubator.quasimondo.com
-     *
-     * created Feburary 29, 2004
-     * Android port : Yahel Bouaziz <yahel at kayenko.com>
-     * http://www.kayenko.com
-     * ported april 5th, 2012
-     *
-     * This is a compromise between Gaussian Blur and Box blur
-     * It creates much better looking blurs than Box Blur, but is
-     * 7x faster than my Gaussian Blur implementation.
-     *
-     * I called it Stack Blur because this describes best how this
-     * filter works internally: it creates a kind of moving stack
-     * of colors whilst scanning through the image. Thereby it
-     * just has to add one new block of color to the right side
-     * of the stack and remove the leftmost color. The remaining
-     * colors on the topmost layer of the stack are either added on
-     * or reduced by one, depending on if they are on the right or
-     * on the left side of the stack.
-     *
-     * If you are using this algorithm in your code please add
-     * the following line:
-     * Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
-     */
-
-    public Bitmap fastblur(Bitmap sentBitmap, float scale, int radius) {
-
-        int width = Math.round(sentBitmap.getWidth() * scale);
-        int height = Math.round(sentBitmap.getHeight() * scale);
-        sentBitmap = Bitmap.createScaledBitmap(sentBitmap, width, height, false);
-
-        Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-
-        if (radius < 1) {
-            return (null);
-        }
-
-        int w = bitmap.getWidth();
-        int h = bitmap.getHeight();
-
-        int[] pix = new int[w * h];
-        Log.e("pix", w + " " + h + " " + pix.length);
-        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-
-        int wm = w - 1;
-        int hm = h - 1;
-        int wh = w * h;
-        int div = radius + radius + 1;
-
-        int r[] = new int[wh];
-        int g[] = new int[wh];
-        int b[] = new int[wh];
-        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
-        int vmin[] = new int[Math.max(w, h)];
-
-        int divsum = (div + 1) >> 1;
-        divsum *= divsum;
-        int dv[] = new int[256 * divsum];
-        for (i = 0; i < 256 * divsum; i++) {
-            dv[i] = (i / divsum);
-        }
-
-        yw = yi = 0;
-
-        int[][] stack = new int[div][3];
-        int stackpointer;
-        int stackstart;
-        int[] sir;
-        int rbs;
-        int r1 = radius + 1;
-        int routsum, goutsum, boutsum;
-        int rinsum, ginsum, binsum;
-
-        for (y = 0; y < h; y++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            for (i = -radius; i <= radius; i++) {
-                p = pix[yi + Math.min(wm, Math.max(i, 0))];
-                sir = stack[i + radius];
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-                rbs = r1 - Math.abs(i);
-                rsum += sir[0] * rbs;
-                gsum += sir[1] * rbs;
-                bsum += sir[2] * rbs;
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-            }
-            stackpointer = radius;
-
-            for (x = 0; x < w; x++) {
-
-                r[yi] = dv[rsum];
-                g[yi] = dv[gsum];
-                b[yi] = dv[bsum];
-
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-
-                if (y == 0) {
-                    vmin[x] = Math.min(x + radius + 1, wm);
-                }
-                p = pix[yw + vmin[x]];
-
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[(stackpointer) % div];
-
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-
-                yi++;
-            }
-            yw += w;
-        }
-        for (x = 0; x < w; x++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            yp = -radius * w;
-            for (i = -radius; i <= radius; i++) {
-                yi = Math.max(0, yp) + x;
-
-                sir = stack[i + radius];
-
-                sir[0] = r[yi];
-                sir[1] = g[yi];
-                sir[2] = b[yi];
-
-                rbs = r1 - Math.abs(i);
-
-                rsum += r[yi] * rbs;
-                gsum += g[yi] * rbs;
-                bsum += b[yi] * rbs;
-
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-
-                if (i < hm) {
-                    yp += w;
-                }
-            }
-            yi = x;
-            stackpointer = radius;
-            for (y = 0; y < h; y++) {
-                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
-                pix[yi] = ( 0xff000000 & pix[yi] ) | ( dv[rsum] << 16 ) | ( dv[gsum] << 8 ) | dv[bsum];
-
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-
-                if (x == 0) {
-                    vmin[y] = Math.min(y + r1, hm) * w;
-                }
-                p = x + vmin[y];
-
-                sir[0] = r[p];
-                sir[1] = g[p];
-                sir[2] = b[p];
-
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[stackpointer];
-
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-
-                yi += w;
-            }
-        }
-
-        Log.e("pix", w + " " + h + " " + pix.length);
-        bitmap.setPixels(pix, 0, w, 0, 0, w, h);
-
-        return (bitmap);
-    }
 }
